6de916181d7c4719075cfbf680ff0b615dd2714e
Ward Cunningham <ward@c2.com>
2017-11-19 06:44:30 -0800

    title shortening, some class cases

--- Cities Alive Ebook to Html/CA_ebook_YK_18.11-web-resources/js/index.js
+++ Cities Alive Ebook to Html/CA_ebook_YK_18.11-web-resources/js/index.js
@@ -10,31 +10,71 @@ function p (i,e) { return e.className + ' ⇒ ' + $(e).text() }
 
 // emit json for body pages
 
-var index = {title:'Index', story:[]}
+var contents = {title:'Contents', story:[]}
 var page = {title:'Prefix', story:[]}
-var site = {index:index}
+var site = {contents:contents}
 
-function begin (e) {
-  title = $(e).text()
+function begin (e,f) {
+  // finish one page
   page.journal = ([{type:'create', item:deepCopy(page), date:Date.now()}])
   slug = asSlug(page.title)
   site[slug] = page
+  // start another
+  longtitle = title = $(e).text().replace(/\d+$/,'')
+  if (f) {
+    item = {type:'markdown', id:id(), text:(f + longtitle)}
+  } else {
+    title = shorten(longtitle)
+    longtitle = longtitle.replace(title, '[['+title+']]')
+    item = {type:'paragraph', id:id(), text:(longtitle)}
+  }
+  contents.story.push(item)
   page = {title: title, story:[]}
-  item = {type:'paragraph', id:id(), text:e.className + ' ⇒ [[' + title + ']]'}
-  index.story.push(item)    
+}
+
+function shorten (longtitle) {
+  if (m = longtitle.match(/^(\d+\. *)?(.*?):/)) { return m[2] }
+  if (m = longtitle.match(/“(.*?)”/)) { return m[1] }
+  if (m = longtitle.match(/^(\d+\. *)?(.*)$/)) { return m[2] }
+  return longtitle
+}
+
+function titlecase (title) {
+  return title.replace(/\b([a-z])/,'\1'.toUpperCase())
 }
 
 $(body).find('p').map(para)
 function para (i,e) {
   switch (e.className) {
   case 'BOOK_Titles_Section-subtitle':
-    begin(e)
+    begin(e,'# ')
     break
   case 'BOOK_Titles_Chapter-title':
   case 'BOOK_Titles_Chapter-Intro--etc':
     begin(e)
     break
+  case 'BOOK_BODY_Quotes_quote-next-page':
+  case 'BOOK_BODY_Quotes_quote-author':
+    item = {type:'html', id:id(), text:('<blockquote>' + $(e).html() + '</blockquote>')}
+    page.story.push(item)
+    break
+  case 'BOOK_BODY_Images_Figure-chapter':
+  case 'BOOK_BODY_Images_Figure-centered':
+    item = {type:'image', id:id(), text:'Lorem Pixel', url:'http://lorempixel.com/420/300/'}
+    page.story.push(item)
+    break
+  case 'BOOK_BODY_Lists_list-references':
+  case 'BOOK_BODY_footnote':
+    item = {type:'html', id:id(), text:('<font size=-1>' + $(e).html() + '</font>')}
+    page.story.push(item)
+    break
+  case 'BOOK_BODY_Lists_list-body':
+  case 'BOOK_BODY_body-text':
+    item = {type:'html', id:id(), text:$(e).html()}
+    page.story.push(item)
+    break
   default:
+    console.log(e.className)
     item = {type:'html', id:id(), text:e.className + ' ⇒ ' + $(e).html()}
     page.story.push(item)    
   }
